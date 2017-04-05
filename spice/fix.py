941a522567078d9d2afa46dede7326cefbc96241
Dave Hawthorne <davehawthorne@ieee.org>
2017-04-05 11:12:07 -0400

    starting on simulation with new design.

--- spice/fix.py
+++ spice/fix.py
@@ -1,66 +1,275 @@
-# fix kicad netlist
-
 import re
+import subprocess as sp
 
-def check(l, n):
-    if len(l) != n:
-        raise Exception("bad count", l, n)
+import numpy as np
+import matplotlib.pyplot as plt
 
-def out(f, l):
-    f.write(" ".join(l) + "\n")
+class LineParser(object):
 
-splitter = re.compile("\s+")
+    def __init__(self):
+        self.designator_re = re.compile('([A-Z]+)(\d+)')
+        self.splitter_re = re.compile("\s+")
 
-with open("heater-control.cir") as in_file, open("hcfix.cir", 'w') as out_file:
-    for line in in_file:
-        line = line.rstrip()
-        line = line.replace("/","_")
-        line = line.replace("Net-_", "n_")
-        line = line.replace("-Pad", "_p")
-        b = splitter.split(line)
 
-        #TEMP!!! print(b)
-        if len(b) == 0 or len(b[0]) == 0:
-           out_file.write("\n")
-        elif b[0][0] == 'J':
-            out_file.write("* " + line + "\n")
-        elif b[0][0] == 'R' and b[0][1] == 'V':
-            out_file.write("* " + line + "\n")
-            check(b, 5)
-            out(out_file, (b[0] + '1', b[1], b[2], '2k'))
-            out(out_file, (b[0] + '2', b[2], b[3], '1k'))
+    def parse_line(self, line):
+        line = line.strip().upper()
+        if line == "":
+            return None
 
-        elif b[0][0] == 'D':
-            check(b, 4)
-            if b[0][1] == 'Z':
-                b[0] = 'A' + b[0]
-            out(out_file, (b[0], b[2], b[1], b[3]))
-        elif b[0][0] == 'Q':
-            check(b, 5)
-            if b[0][1] == 'N':
-                b[0] = 'X' + b[0]
-            out(out_file, (b[0], b[2], b[1], b[3], b[4]))
-        elif b[-1] == 'LM324':
-            check(b, 16)
-            n = lambda i: b[0] + str(i)
-            out(out_file, (n(1), b[3], b[2], b[4], b[11], b[1], 'LM324q'))
-            out(out_file, (n(2), b[5], b[6], b[4], b[11], b[7], 'LM324q'))
-            out(out_file, (n(3), b[10], b[9], b[4], b[11], b[8], 'LM324q'))
-            out(out_file, (n(4), b[12], b[13], b[4], b[11], b[14], 'LM324q'))
+        elif line[0] in ".*+":
+            return None
+
+        bits = self.splitter_re.split(line)
+
+        match = self.designator_re.match(bits[0])
+
+
+        device_type, device_num = match.groups()
+
+        return (device_type, device_num, bits[1:])
+
+
+class Device(object):
+
+    value_re = re.compile('^(\d+)([A-Z]+)$')
+    net_re = re.compile('^NET-_(\w+)-PAD(\d+)_$')
+
+    def line(*args):
+        fields = []
+        for arg in args:
+            if type(arg) == list:
+                fields.extend(arg)
+            else:
+                fields.append(arg)
+
+        return " ".join(["{:8}".format(field) for field in fields])
+
+    def fix_net(name):
+        if name[0] == '/':
+            return name[1:]
+        if name in ['GND', 'VCC']:
+            return name
+        dev, pad = Device.net_re.match(name).groups()
+        return "n_{}_p{}".format(dev, pad)
+
+    def __init__(self, device_type, device_num, nets, params):
+        self._device_type = device_type
+        self._device_num = device_num
+        self._nets = [Device.fix_net(net) for net in nets]
+        self._params = params
+
+    def designator(self, extra=''):
+        return self._device_type + self._device_num + extra
+
+    def write(self):
+        return [Device.line(self.designator(), self._nets, self._params)]
+
+
+class Module(Device):
+    def __init__(self, device_type, device_num, parts):
+        Device.__init__(self, device_type, device_num, parts[0:-1], [parts[-1]])
+
+
+    def designator(self, extra=''):
+        return 'X' + self._device_type + self._device_num + extra
+
+    def write(self):
+        return [Device.line(self.designator(), self._nets, self._params)]
+
+
+class DualOpamp(Module):
+    def __init__(self, device_type, device_num, parts):
+        if len(parts) != 9:
+            raise Exception('bad count', device_type, device_num, parts)
+        Device.__init__(self, device_type, device_num, parts[0:-1], [parts[-1]])
+
+    def write(self):
+        n = self._nets
+        vcc = n[7]
+        gnd = n[3]
+        param = self._params[0]
+        return [
+            Device.line(self.designator('a'), n[2], n[1], vcc, gnd, n[0],  param),
+            Device.line(self.designator('b'), n[4], n[5], vcc, gnd, n[6],  param)]
+
+
+class TwoTerm(Device):
+    def __init__(self, device_type, device_num, parts):
+        if len(parts) != 3:
+            raise Exception('bad count', device_type, device_num, parts)
+        Device.__init__(self, device_type, device_num, [parts[1], parts[0]], parts[2:])
+
+
+class Passive(TwoTerm):
+    def adjust(self, value):
+        self._params[0] = str(value)
+
+
+class Zener(TwoTerm):
+    def designator(self, extra=''):
+        return "A" + self._device_num + extra
+
+class Transistor(Device):
+    def __init__(self, device_type, device_num, parts):
+        if len(parts) != 4:
+            raise Exception('bad count', device_type, device_num, parts)
+        Device.__init__(self, device_type, device_num, parts[0:3], [parts[3]])
+
+class TransModule(Device):
+    def __init__(self, device_type, device_num, parts):
+        if len(parts) != 4:
+            raise Exception('bad count', device_type, device_num, parts)
+        Device.__init__(self, device_type, device_num, [parts[1], parts[0], parts[2]], [parts[3]])
+
+    def designator(self, extra=''):
+        return "XQ" + self._device_num + extra
+
+class Potentiometer(Device):
+
+    def __init__(self, device_type, device_num, parts):
+        if len(parts) != 4:
+            raise Exception('bad count', device_type, device_num, parts)
+        match = Device.value_re.match(parts[3])
+        res_str, self._units = match.groups()
+        self._res = float(res_str)
+        self._position = 0.5
+        Device.__init__(self, device_type, device_num, parts[0:3], None)
+
+
+    def adjust(self, position):
+        self._position = position
+
+
+    def write(self):
+        res_a = str(self._res * (1.0 - self._position)) + self._units
+        res_b = str(self._res * self._position) + self._units
+
+        return [
+            Device.line(self.designator('a'), self._nets[0], self._nets[1],  res_a),
+            Device.line(self.designator('b'), self._nets[1], self._nets[2],  res_b)]
+
+
+def module(device_type, device_num, parts):
+    param = parts[-1]
+    if param in ['LM358']:
+        return DualOpamp(device_type, device_num, parts)
+    #if param in ['TL431LP']:
+
+    return Module(device_type, device_num, parts)
+
+
+def transistor(device_type, device_num, parts):
+    param = parts[-1]
+    if param in ['IRF540N']:
+        return TransModule(device_type, device_num, parts)
+    else:
+        return Transistor(device_type, device_num, parts)
+
+
+class KicadSpiceFix(object):
+    """text
+    """
+
+
+
+    class_map = {
+        'R': TwoTerm,
+        'C': TwoTerm,
+        'L': TwoTerm,
+        'D': TwoTerm,
+        'Q': transistor,
+        'U': module,
+        'DZ': Zener,
+        'RV': Potentiometer,
+        'J': None}
+
+
+    def __init__(self, filename):
+        self._subs = {}
+        self._filename = filename
+        self._devices = {}
+        self._extras = {}
+        parser = LineParser()
+        with open(filename) as in_file:
+            for line in in_file:
+                fields = parser.parse_line(line)
+                if fields is None:
+                    continue
+                des_type, des_num, parts = fields
+                dev_class = KicadSpiceFix.class_map[des_type]
+                if dev_class is None:
+                    continue
+
+                try:
+                    self._devices[des_type + des_num] = dev_class(des_type, des_num, parts)
+                except Exception as e:
+                    print(des_type, des_num, e)
+
+
+    def device(self, designator):
+        return self._devices[designator]
+
+    def set_lines(self, section, lines):
+        self._extras[section] = lines
+
+    def write(self, filename):
+        lines = ['* {}'.format(self._filename), '']
+        for dev in self._devices.values():
+            lines.extend(dev.write())
+        for extras in self._extras.values():
+            lines.extend(extras)
+        lines.extend(["", ".end"])
+        with open(filename, 'w') as out_file:
+            for line in lines:
+                print(line, file=out_file)
+
+
+    def write_and_run(self, filename):
+        self.write(filename)
+        process = sp.Popen(['ngspice', '-b', 't2.cir'], stdout=sp.PIPE, stderr=sp.PIPE)
+        stdout, stderr = process.communicate()
+        print("#" * 10 + "STDOUT" + "#" * 10)
+        print(stdout.decode('utf-8'))
+        print("#" * 10 + "STDERR" + "#" * 10)
+        print(stderr.decode('utf-8'))
+
+
+def make_meas_names(field_names):
+    out = []
+    for name in field_names:
+        if name[0] == 'i':
+            out.append("i(v{})".format(name[1:]))
         else:
-            out_file.write(line + "\n")
+            out.append(name)
+    return " ".join(out)
 
-pre1 = """
-VBATT VCC GND DC 12 PULSE(12 10 1 1e-4 1e-4 1 10)
-VEN  _enable GND 5
-RLOAD VCC _load 2
 
-"""
 
-pre2 = """
-VBATT VCC GND DC 12
-VEN  _enable GND 0 PULSE(0 5 1 1e-5 1e-5 1 10)
-RLOAD VCC _load 2
+def read_wrdata_file(filename, nodes):
 
-"""
+    i_re = re.compile("^i\(v(\w+)\)$", flags=re.IGNORECASE)
 
+    array = np.loadtxt(filename)
+    V = {}
+    I = {}
+    for i, name in enumerate(nodes.split()):
+        vector = array[:, 1 + 2 * i]
+        match = i_re.match(name)
+        if match:
+            I[match.group(1)] = vector
+        else:
+            V[name] = vector
+
+    return array[:, 0], V, I
+
+
+def plot_all(Vc, V, I):
+    plt.figure()
+    plt.subplot(2,1,1)
+    for name, vector in V.items():
+        plt.plot(Vc, vector, label="V"+name)
+    plt.legend()
+    plt.subplot(2,1,2)
+    for name, vector in I.items():
+        plt.plot(Vc, vector, label="I"+name)
+    plt.legend()
